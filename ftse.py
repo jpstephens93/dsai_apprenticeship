cd1bd5615d46755569dc62312d6aeead2364f10b
Jack <jackstephens93@hotmail.co.uk>
2023-12-31 16:20:42 +0000

    finished ftse for submission - corrections to wrong answers

--- ftse.py
+++ ftse.py
@@ -4,16 +4,16 @@ df = pd.read_csv('data/FTSE100.csv')
 df.head()
 
 # 1) tidy data
-df_clean = df.copy()
+clean_df = df.copy()
 
-mask = df_clean['Ticker'] == 'RDSA'
-rdsa_index = df_clean[mask].index
+mask = clean_df['Ticker'] == 'RDSA'
+rdsa_index = clean_df[mask].index
 
-df_clean.drop(rdsa_index, inplace=True)
-df_clean.drop(columns='Strong Buy', inplace=True)
+clean_df.drop(rdsa_index, inplace=True)
+clean_df.drop(columns='Strong Buy', inplace=True)
 
 # 2) col data type
-price_df = df_clean.copy()
+price_df = clean_df.copy()
 price_df['Mid-price (p)'] = price_df['Mid-price (p)'].str.replace(',', '').astype(float)
 
 
@@ -21,12 +21,14 @@ price_df['Mid-price (p)'] = price_df['Mid-price (p)'].str.replace(',', '').astyp
 # (a)
 def format_change(string):
     if string[-1] == '%':
-        return float(string[:-1])
-    else:
-        return float(string) * 100
+        string = string[:-1]
+    flt = float(string)
+    if flt > 0:
+        flt = flt * 100
+    return flt
 
 
-string = '-0.015'
+string = '0.45%'
 format_change(string)
 
 # (b)
@@ -41,8 +43,8 @@ print(holding[1])
 print(holding[2])
 
 holding_dict = {
-    'holding_cost': holding[1] * holding[2],
-    'holding_value': holding[1] * change_df[change_df['Ticker'] == holding[0]]['Mid-price (p)'].values[0],
+    'holding_cost': holding[1] * (holding[2] / 100),
+    'holding_value': holding[1] * (change_df[change_df['Ticker'] == holding[0]]['Mid-price (p)'].values[0] / 100),
     'change_in_value': 100 * ((
         (holding[1] * change_df[change_df['Ticker'] == holding[0]]['Mid-price (p)'].values[0]) / (holding[1] * holding[2])
     ) - 1)
@@ -62,6 +64,5 @@ companies_list = []
 for company, price in watchlist:
     # print([company, price])
     company_row = comparison_df[comparison_df['Company'] == company]
-    if price <= company_row['Mid-price (p)'].values[0] or company_row['Buy Ratio'].values[0] >= 0.5:
+    if company_row['Mid-price (p)'].values[0] <= price or company_row['Buy Ratio'].values[0] >= 0.5:
         companies_list.append(company)
-
