5d1dccf8a9e0574a118837aeca9f4d2db467f96d
lammascapital <lammascapital@gmail.com>
2024-02-02 17:56:53 +0000

    starting real estate analysis

---
+++ Module 2 - Introduction to Machine Learning/real_estate_analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Module 2 - Introduction to Machine Learning/data/real_estate_valuation_data_set.csv')
+df.head()
+
+# Q1.
+df.info()
+values_missing =
+
